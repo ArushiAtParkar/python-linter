615ada2f92b1046fa134fa6f74da82537ae276f3
Arushi <asaxena@parkar.digital>
2025-04-14 12:47:27 +0530

    added python script and GitHub Action workflows

---
+++ script.py
@@ -0,0 +1,6 @@
+def greet(name):
+    """Function to greet a person."""
+    return f"Hello, {name}!"
+
+greet("World")
+# This is a simple script that defines a function to greet a person by name.    
